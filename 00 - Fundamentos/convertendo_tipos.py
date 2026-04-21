f7f0a471fd60b355756eda5e38a388a3c54d99e6
Marcelo JS Santos <marcelojssantos2012@gmail.com>
2026-04-21 14:30:55 +0000

    Playground tipos, variáveis e contantes e input

--- 00 - Fundamentos/convertendo_tipos.py
+++ 00 - Fundamentos/convertendo_tipos.py
@@ -1,20 +1,12 @@
-print(int(1.97348728))
-print(int("10"))
-print(float("10.10"))
-print(float(100))
+print(f'Convertendo o float 1.97348728 para um inteiro: {1.97348728} -> {int(1.97348728)}')
+print(f'Convertendo o texto "10" para um inteiro: {int("10")}')
+print(f'Convertendo o texto "10.10" para um float: {float("10.10")}')
+print(f'Convertendo o inteiro 100 para um float: {float(100)}')
 
 valor = 10
 valor_str = str(valor)
-print(type(valor))
-print(type(valor_str))
+print(f"Qual o tipo de 'valor': {type(valor)}")
+print(f"Qual o tipo de 'valor': {type(valor_str)}")
 
-print(100 / 2)
-print(100 // 2)
-
-boleano = True
-print(boleano)
-
-texto_1 = "Python 1"
-texto_2 = 'Python 2'
-print(texto_1)
-print(texto_2)
+print(f"Dividinfo 2 números inteiros por '/' resulta em um float:  {100 / 2}")
+print(f"Dividinfo 2 números inteiros por '//' resulta em um inteiro:  {100 // 2}")
--- 00 - Fundamentos/variaveis_constantes.py
+++ 00 - Fundamentos/variaveis_constantes.py
@@ -1,12 +1,19 @@
 nome = "Guilherme"
 idade = 28
 
+#as variáveis nome e idade foram alteradaa
 nome, idade = "Giovanna", 27
 
 print(nome, idade)
+print(f'A {nome} tem {idade} anos de idade.')
 
 limite_saque_diario = 1000
+print(f'O seu limite diário de saque é {limite_saque_diario}.')
 
+#A constante não deve ser alterada
 BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+print(BRAZILIAN_STATES)
 
+#Mas em Python as constantes podem ser alteradas, MAS NÂO SÃO UMA BOA PRÁTICA
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS", "BA", "MG"]
 print(BRAZILIAN_STATES)
